c9aba4d09e677476b24f1c5a66390dd7ffc1bc1e
JPSimkin <147119375+JPSimkin@users.noreply.github.com>
2024-01-27 02:50:51 +0000

    modified:   _03_06/EnhancedForLoops.java

--- _03_06/EnhancedForLoops.java
+++ _03_06/EnhancedForLoops.java
@@ -14,14 +14,14 @@ public class EnhancedForLoops {
 
     List<String> weekDays = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
     // Write an enhanced for loop to print out each week day in the list.
-    for(String weekDay:weekDays) {
+    for(String weekDay : weekDays) {
       System.out.println(weekDay);
     }
 
     int[] randomNumbers = { 23, 51, 72, 84, 1, 60, 34, 102 };
     // Write an enhanced for loop to print out the numbers in the array that are
     // greater than 50.
-    for (int randomNumber: randomNumbers) {
+    for (int randomNumber : randomNumbers) {
       if (randomNumber > 50) {
         System.out.println(randomNumber);
       }
